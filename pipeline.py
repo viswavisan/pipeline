ece2ed7c1181565de934485829c766da924e6d64
viswanathanl <viswanathanl@chola.murugappa.com>
2025-02-19 17:48:48 +0530

    update

--- pipeline.py
+++ pipeline.py
@@ -8,17 +8,17 @@ from kubernetes import client, config
 class deployment_pipeline:
     def __init__(self):
         
-        self.build='local'
+        self.build='ECR'
         if self.build=='local':self.container_path=''
         else:self.container_path="ghcr.io/viswavisan/"
-        self.deployment_type='docker'
+        self.deployment_type='kubernets'
 
-        self.REPO_NAME='react_demo1'
+        self.REPO_NAME='fastapi-demo1'
         self.REPO_URL=f"https://github.com/viswavisan/{self.REPO_NAME}.git"
         self.BRANCH='main'
 
         self.expose_port=8000
-        self.port=80 #(for python 8000/ for node 80)
+        self.port=8000
 
         self.image_name=f'{self.container_path}{self.REPO_NAME}:latest' 
         print(self.image_name)
@@ -51,16 +51,21 @@ class deployment_pipeline:
     def build_latest_image(self):
         print('building image...')
         try:
-            self.d_client.images.build(path='./'+self.REPO_NAME, tag=self.image_name)
+            # self.d_client.images.build(path='./'+self.REPO_NAME, tag=self.image_name)
+            image, build_logs = self.d_client.images.build(path='./' + self.REPO_NAME, tag=self.image_name)
+            for log in build_logs:print(log)
+            print(image)
         except Exception as e: print(str(e))
 
         if self.build!='local':
-            # self.d_client.login(username='viswavisan', password='', registry='ghcr.io') 
+            # self.d_client.login(username='viswavisan', password='1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', registry='ghcr.io') 
+            os.system('echo 1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 | docker login ghcr.io -u viswavisan --password-stdin')
             push_response = self.d_client.images.push(self.image_name, stream=True, decode=True)
             for line in push_response:print(line) 
         
     def deploy_in_docker(self):
         print('deployment started in docker')
+
         try:
             existing_container = self.d_client.containers.get(self.container_name)
             if existing_container:existing_container.remove(force=True)
@@ -73,43 +78,46 @@ class deployment_pipeline:
                         container.remove(force=True)
         except:pass
 
-        self.d_client.containers.run(self.app_name, detach=True, name=self.container_name, ports={f'{self.port}/tcp': self.expose_port})
+        self.d_client.containers.run(self.image_name, detach=True, name=self.container_name, ports={f'{self.port}/tcp': self.expose_port})
 
     def kubernet_deployment(self):
-        print('deployment start...')
-        config.load_kube_config()
-        core_v1 = client.CoreV1Api()  
-    
-        if self.namespace not in [ns.metadata.name for ns in core_v1.list_namespace().items]:
-            print('creating namespace')
-            namespace_body = client.V1Namespace(metadata=client.V1ObjectMeta(name=self.namespace))
-            core_v1.create_namespace(body=namespace_body)
+        try:
+            print('deployment start...')
 
-        apps_v1 = client.AppsV1Api()
-        if self.deployment_name in [deployment.metadata.name for deployment in apps_v1.list_namespaced_deployment(self.namespace).items]:
-            print('updating deployment')
-            deployment = apps_v1.read_namespaced_deployment(self.deployment_name, self.namespace)
-            deployment.spec.template.spec.containers[0].image = self.image_name
-            deployment = apps_v1.patch_namespaced_deployment( name=self.deployment_name, namespace=self.namespace, body=deployment )
-        else:
-            print('creating new deployment')
-            container = client.V1Container(name=self.container_name,image=self.image_name,ports=[client.V1ContainerPort(container_port=self.port)],image_pull_policy='Never')
-            pod_template = client.V1PodTemplateSpec(metadata=client.V1ObjectMeta(labels={"app":self.app_name}),spec=client.V1PodSpec(containers=[container]))
-            deployment_spec = client.V1DeploymentSpec(replicas=1,template=pod_template,selector=client.V1LabelSelector(match_labels={"app":self.app_name}))
-            deployment = client.V1Deployment(api_version="apps/v1",kind="Deployment",metadata=client.V1ObjectMeta(name=self.deployment_name),spec=deployment_spec)
+            config.load_kube_config()
+            core_v1 = client.CoreV1Api()  
+            print([ns.metadata.name for ns in core_v1.list_namespace().items])
+            if self.namespace not in [ns.metadata.name for ns in core_v1.list_namespace().items]:
+                print('creating namespace')
+                namespace_body = client.V1Namespace(metadata=client.V1ObjectMeta(name=self.namespace))
+                core_v1.create_namespace(body=namespace_body)
 
-        if self.service_name not in [service.metadata.name for service in core_v1.list_namespaced_service(self.namespace).items]:
-            print('creating new service')
-            apps_v1.create_namespaced_deployment(namespace=self.namespace, body=deployment)
-            service = client.V1Service(api_version="v1",
-                                    kind="Service",
-                                    metadata=client.V1ObjectMeta(name=self.service_name),
-                                    spec=client.V1ServiceSpec(selector={"app":self.app_name},
-                                                                ports=[client.V1ServicePort(port=self.port, target_port=self.port)],
-                                                                type="NodePort"))
-            core_v1 = client.CoreV1Api()
-            core_v1.create_namespaced_service(namespace=self.namespace, body=service)
+            apps_v1 = client.AppsV1Api()
+            if self.deployment_name in [deployment.metadata.name for deployment in apps_v1.list_namespaced_deployment(self.namespace).items]:
+                print('updating deployment')
+                deployment = apps_v1.read_namespaced_deployment(self.deployment_name, self.namespace)
+                deployment.spec.template.spec.containers[0].image = self.image_name
+                deployment = apps_v1.patch_namespaced_deployment( name=self.deployment_name, namespace=self.namespace, body=deployment )
 
+            else:
+                print('creating new deployment')
+                container = client.V1Container(name=self.container_name,image=self.image_name,ports=[client.V1ContainerPort(container_port=self.port)],image_pull_policy='Always')
+                pod_template = client.V1PodTemplateSpec(metadata=client.V1ObjectMeta(labels={"app":self.app_name}),spec=client.V1PodSpec(containers=[container]))
+                deployment_spec = client.V1DeploymentSpec(replicas=1,template=pod_template,selector=client.V1LabelSelector(match_labels={"app":self.app_name}))
+                deployment = client.V1Deployment(api_version="apps/v1",kind="Deployment",metadata=client.V1ObjectMeta(name=self.deployment_name),spec=deployment_spec)
+                # print(deployment)
+            if self.service_name not in [service.metadata.name for service in core_v1.list_namespaced_service(self.namespace).items]:
+                print('creating new service')
+                apps_v1.create_namespaced_deployment(namespace=self.namespace, body=deployment)
+                service = client.V1Service(api_version="v1",
+                                        kind="Service",
+                                        metadata=client.V1ObjectMeta(name=self.service_name),
+                                        spec=client.V1ServiceSpec(selector={"app":self.app_name},
+                                                                    ports=[client.V1ServicePort(port=self.port, target_port=self.expose_port)],
+                                                                    type="NodePort"))
+                core_v1 = client.CoreV1Api()
+                core_v1.create_namespaced_service(namespace=self.namespace, body=service)
+        except Exception as e:print(str(e))
     def check_service_status(self):
         pods = client.CoreV1Api().list_namespaced_pod(self.namespace, label_selector=f'app={self.app_name}')
         for pod in pods.items:
